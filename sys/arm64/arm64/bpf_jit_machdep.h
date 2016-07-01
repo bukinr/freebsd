41f93a1b09142bd82ac04f2137c591a9a8c79660
Ruslan Bukin <br@bsdpad.com>
2016-07-01 10:26:00 +0000

    cleanup

--- sys/arm64/arm64/bpf_jit_machdep.h
+++ sys/arm64/arm64/bpf_jit_machdep.h
@@ -45,6 +45,44 @@
 #define	A64_LR		A64_R(30)
 #define	A64_SP		A64_R(31)
 
+/*
+ * Instructions assembly parts
+ */
+#define	OPC_S		30
+#define	OPC_LDP		0b10
+#define	OPC_STP		0b10
+#define	RT2_S		10
+#define	RT1_S		0
+
+#define	RM_S		16
+#define	RD_S		0
+#define	RT_S		0
+#define	RA_S		10	/* Addend shift */
+#define	RN_S		5
+#define	IMM6_S		10
+#define	IMM7_S		15
+#define	IMM9_S		12
+#define	IMM12_S		10
+#define	IMM16_S		5
+#define	IMM19_S		5
+#define	IMM26_S		0
+#define	IMMR_S		16
+#define	IMMS_S		10
+#define	IMM_N		(1 << 22)
+
+#define	MOVE_WIDE_OP_S	29
+#define	MOVE_WIDE_OP_N	0
+#define	MOVE_WIDE_OP_Z	2
+#define	MOVE_WIDE_OP_K	3
+#define	HW_S		21
+
+#define	SHIFT_LSL	0
+#define	SHIFT_LSR	1
+#define	SHIFT_ASR	10
+#define	SHIFT_S		22
+
+/* Condition codes */
+#define	COND_S		0
 #define	COND_EQ		0b0000
 #define	COND_NE		0b0001
 #define	COND_CS		0b0010
