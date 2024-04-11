39347a0b904ca160dde0a1529a65ceb64756b1d5
Ruslan Bukin <br@bsdpad.com>
2024-04-11 15:50:34 +0100

    improve script

--- build.sh
+++ build.sh
@@ -5,4 +5,8 @@ fi
 
 make -j24 TARGET=riscv KERN${A}=GENERIC buildkernel || exit 1
 
-sh embed.sh
+sh embed.sh || exit 2
+
+ADDR=`sh call_hyp.sh | awk '{print $1}'`
+echo until pc 0 ${ADDR}
+echo bhyve -o bootrom=/bin/ls test
