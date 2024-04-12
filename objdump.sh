caa7a901d04e33cd7e089e15beb10371c71aa573
Ruslan Bukin <br@bsdpad.com>
2024-04-12 15:28:00 +0100

    use system objdump

--- objdump.sh
+++ objdump.sh
@@ -1,1 +1,2 @@
-riscv64-unknown-freebsd15.0-objdump -d /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel | less -S
+#riscv64-unknown-freebsd15.0-objdump -d /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel | less -S
+objdump -d /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel | less -S
