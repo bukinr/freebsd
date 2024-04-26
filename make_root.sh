52209d7a91c2dcabd72ff1a2931552fe95f1f976
Ruslan Bukin <br@bsdpad.com>
2024-04-26 23:39:22 +0100

    increase image size

--- make_root.sh
+++ make_root.sh
@@ -1,1 +1,1 @@
-tools/tools/makeroot/makeroot.sh -s 38m -e generic/extras -f bhyve.files mdroot.img /home/br/riscv-world/
+tools/tools/makeroot/makeroot.sh -s 48m -e generic/extras -f bhyve.files mdroot.img /home/br/riscv-world/
