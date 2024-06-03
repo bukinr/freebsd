ff82020ed74c6cb9074aa7180e16aa1a408424ed
Ruslan Bukin <br@bsdpad.com>
2024-06-03 14:48:47 +0100

    change path to kernel

---
+++ call_hyp.sh
@@ -0,0 +1,1 @@
+objdump -d /usr/obj/usr/home/br/dev/freebsd/sys/GENERIC/kernel | grep call_hyp
--- embed.sh
+++ embed.sh
@@ -1,3 +1,3 @@
-sh ./sys/tools/embed_mfs.sh /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel /home/br/dev/freebsd/mdroot.img
+sh ./sys/tools/embed_mfs.sh /usr/obj/usr/home/br/dev/freebsd/sys/GENERIC/kernel /home/br/dev/freebsd/mdroot.img
 
-riscv64-unknown-freebsd15.0-objcopy -O binary /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel.bin
+riscv64-unknown-freebsd15.0-objcopy -O binary /usr/obj/usr/home/br/dev/freebsd/sys/GENERIC/kernel /usr/obj/usr/home/br/dev/freebsd/sys/GENERIC/kernel.bin
