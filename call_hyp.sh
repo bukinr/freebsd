37d2fefd89a1d8a40ec31b2ccd95fb2ec15d4072
Ruslan Bukin <br@bsdpad.com>
2024-08-07 14:08:29 +0100

    fix grep cmd

--- call_hyp.sh
+++ call_hyp.sh
@@ -1,1 +1,1 @@
-objdump -d /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel | grep call_hyp
+objdump -d /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel | grep "<vmm_switch>:"
