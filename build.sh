ef3ac40fba75a3ee2c23e503fbfd6a3c6528b1c9
Ruslan Bukin <br@bsdpad.com>
2025-02-24 14:19:01 +0000

    relocate dtb

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ cp /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel /tftpboot/c
 
 riscv64-unknown-freebsd15.0-objcopy -O binary /tftpboot/codasip_vcu118/kernel /tftpboot/codasip_vcu118/kernel.bin
 
-echo "setenv serverip 10.5.0.1; setenv ipaddr 10.5.0.93; tftp 0x82000000 codasip_vcu118/kernel.bin; go 0x82000000 0 \$fdt_addr"
+echo fdt move \$fdt_addr 0x88000000
+echo "setenv serverip 10.5.0.1; setenv ipaddr 10.5.0.93; tftp 0x82000000 codasip_vcu118/kernel.bin; go 0x82000000 0 0"
 
 echo fatload mmc 0 0x82000000 loader_lua.efi
