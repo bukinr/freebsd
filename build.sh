c6d4eae053e9c7f40ad77ebb0bc164a2241e6635
Ruslan Bukin <br@bsdpad.com>
2025-03-02 16:13:48 +0000

    fix script

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ cp /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel /tftpboot/c
 
 riscv64-unknown-freebsd15.0-objcopy -O binary /tftpboot/codasip_vcu118/kernel /tftpboot/codasip_vcu118/kernel.bin
 
-echo fdt move \$fdt_addr 0x88000000
-echo "setenv serverip 10.5.0.1; setenv ipaddr 10.5.0.93; tftp 0x82000000 codasip_vcu118/kernel.bin; go 0x82000000 0 0"
+# echo fdt move \$fdt_addr 0x88000000
+# echo "setenv serverip 10.5.0.1; setenv ipaddr 10.5.0.93; tftp 0x82000000 codasip_vcu118/kernel.bin; go 0x82000000 0 0"
 
-echo fatload mmc 0 0x82000000 loader_lua.efi
+echo "fdt move \$fdt_addr 0x88000000; setenv serverip 10.5.0.1; setenv ipaddr 10.5.0.93; tftp 0x82000000 codasip_vcu118/kernel.bin; go 0x82000000 0 0"
+
+# echo fatload mmc 0 0x82000000 loader_lua.efi
