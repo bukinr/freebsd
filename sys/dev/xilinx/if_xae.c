ca58b94941ed7a9614e28c361d5f13f4e06f56a2
Ruslan Bukin <br@bsdpad.com>
2025-03-26 14:52:37 +0000

    Fix vmem allocation

--- sys/dev/xilinx/if_xae.c
+++ sys/dev/xilinx/if_xae.c
@@ -97,6 +97,7 @@
 #define	NUM_RX_MBUF		16
 #define	BUFRING_SIZE		8192
 #define	MDIO_CLK_DIV_DEFAULT	29
+#define	BUF_NPAGES		512
 
 #define	PHY1_RD(sc, _r)		\
 	xae_miibus_read_reg(sc->dev, 1, _r)
@@ -837,6 +838,8 @@ setup_xdma(struct xae_softc *sc)
 {
 	device_t dev;
 	vmem_t *vmem;
+	vm_paddr_t phys;
+	vm_page_t m;
 	int error;
 
 	dev = sc->dev;
@@ -890,17 +893,13 @@ setup_xdma(struct xae_softc *sc)
 	/* Setup bounce buffer */
 	vmem = xdma_get_memory(dev);
 	if (!vmem) {
-		vm_page_t m;
-		vm_paddr_t phys;
-
 		m = vm_page_alloc_noobj_contig(VM_ALLOC_WIRED | VM_ALLOC_ZERO,
-		    512, 0, BUS_SPACE_MAXADDR_32BIT, PAGE_SIZE, 0,
+		    BUF_NPAGES, 0, BUS_SPACE_MAXADDR_32BIT, PAGE_SIZE, 0,
 		    VM_MEMATTR_DEFAULT);
 		phys = VM_PAGE_TO_PHYS(m);
-
 		vmem = vmem_create("xDMA vmem", 0, 0, PAGE_SIZE, PAGE_SIZE,
 		    M_BESTFIT | M_WAITOK);
-		vmem_add(vmem, phys, 512 * PAGE_SIZE, 0);
+		vmem_add(vmem, phys, BUF_NPAGES * PAGE_SIZE, 0);
 	}
 
 	xchan_set_memory(sc->xchan_tx, vmem);
