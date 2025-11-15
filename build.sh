4f5febf4f3f5a16657a7de919f15c2330df9136c
Ruslan Bukin <br@bsdpad.com>
2025-11-15 12:21:24 +0000

    add hint

--- build.sh
+++ build.sh
@@ -13,4 +13,6 @@ riscv64-unknown-freebsd15.0-objcopy -O binary /tftpboot/codasip_vcu118/kernel /t
 
 echo "setenv serverip 10.5.0.1; setenv ipaddr 10.5.0.93; tftp 0x88000000 codasip_vcu118/x730-mp1-hobgoblin-vcu118.dtb; tftp 0x82000000 codasip_vcu118/loader_lua.efi; bootefi 0x82000000 0x88000000"
 
+echo "setenv serverip 10.5.0.1; setenv ipaddr 10.5.0.93; tftp 0x82000000 codasip_vcu118/loader_lua.efi; bootefi 0x82000000 0x17eb4f4b0"
+
 # echo fatload mmc 0 0x82000000 loader_lua.efi
