1ee78d4a57d824795708267c673724e062c59e53
Ruslan Bukin <br@bsdpad.com>
2025-11-17 21:41:11 +0000

    fix path

--- embed.sh
+++ embed.sh
@@ -1,1 +1,1 @@
-sh ./sys/tools/embed_mfs.sh /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel /home/br/dev/freebsd/mdroot-small.img
+sh ./sys/tools/embed_mfs.sh /usr/obj/usr/home/br/dev/freebsd/riscv.riscv64/sys/GENERIC/kernel /home/br/dev/freebsd/mdroot.img
